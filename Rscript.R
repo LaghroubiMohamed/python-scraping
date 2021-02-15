a1b6fed6fbfdf7a2bab024714fd99eec52c2b0d9
glitcher <laghroubi,mohamed@gmail.com>
2021-02-15 23:33:07 +0100

    add the chromedriver and Rscipt

--- Rscript.R
+++ Rscript.R
@@ -11,3 +11,23 @@ is.numeric.Date(coin$date)
 typeof(coin)
 coin$mean <- rowMeans(coin[,c('high', 'low')], na.rm=TRUE)
 coin$mean
+
+
+X<- data.frame(Placebo=c(5,8,7,7,10,8),
+               T2=c(4,6,6,3,5,6),
+               T3=c(6,4,4,5,4,3),
+               T4=c(7,4,6,6,3,5),
+               T5=c(9,3,5,7,7,6))
+delai <- stack(X)$values
+traitement <- stack(X)$ind
+traitement
+tapply(delai, traitement, summary)
+plot(delai~traitement)
+mon.aov <- aov(delai~traitement)
+summary(mon.aov)
+lm(formula=delai ~ traitement)
+par(mfrow=c(2,2))
+plot(lm(mon.aov),col.smouth="red")
+bartlett.test(delai~traitement)
+bartlett.test(BWT ~ as.factor(SMOKE))
+levene.test(delai~traitement)
